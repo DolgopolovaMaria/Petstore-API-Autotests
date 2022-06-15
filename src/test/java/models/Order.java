55e273a0b536090d577db80c2675e3df4a5b9751
mariya.dolgopolova <dolgopolovamariia@gmail.com>
2022-06-15 16:52:43 +0300

    models

---
+++ src/test/java/models/Order.java
@@ -0,0 +1,15 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Order {
+    private Integer id;
+    private Integer petId;
+    private Integer quantity;
+    private String shipDate;
+    private String status;
+    private Boolean complete;
+}
---
+++ src/test/java/models/User.java
@@ -0,0 +1,17 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private Integer id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Integer userStatus;
+}
